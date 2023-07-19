0de953a22bd79e8a1be92dfc665c387dd32aff7e
mecachet <otoqvilitaia@gmail.com>
2023-07-19 08:26:41 +0400

    added code in js

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("JavaScript file is linked correctly. Hooray.");
+const numberArrays = [2, 4, 6, 8, 10, 12];
+numberArrays.pop();
+numberArrays.unshift(12);
+console.log(numberArrays);
+const productList = [
+  { productTitle: "Heater", productPrice: 25 },
+  { productTitle: "Filter", productPrice: 80 },
+  { productTitle: "Subtrate", productPrice: 40 },
+];
+console.log(productList);

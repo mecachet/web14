eefe2407e764eb8f678a8f99bac6dd195924b59d
mecachet <otoqvilitaia@gmail.com>
2023-07-19 08:31:44 +0400

    Added more JS code,
    added products, const phone and more.

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ numberArrays.pop();
 numberArrays.unshift(12);
 console.log(numberArrays);
 const productList = [
-  { productTitle: "Heater", productPrice: 25 },
-  { productTitle: "Filter", productPrice: 80 },
-  { productTitle: "Subtrate", productPrice: 40 },
+  { productTitle: "USB Drive", productPrice: 25 },
+  { productTitle: "Wireless Charger", productPrice: 80 },
+  { productTitle: "WhyPhone", productPrice: 1140 },
 ];
 console.log(productList);
+const numbers = [46, 122, 74, 8, 64];
+const average =
+  (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]) /
+  numbers.length;
+console.log(average);
+const phone = {
+  phoneName: "Google Pixel",
+  phoneDescription: "Android OS",
+  phonePrice: 1100,
+  shopAddresses: ["Tsereteli Av, Kidobani"],
+  phoneMemoryInGB: 256,
+};
+console.log(phone);
+console.log(
+  `Phone model is ${phone.phoneName}, the price is ${phone.phonePrice}, and you can purchase it at ${phone.shopAddresses[0]}.`
+);
